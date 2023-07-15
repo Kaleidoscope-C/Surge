05045508b12e72e57c9b83567ca0419c50fc7388
Kaleidoscope-C <52686914+Kaleidoscope-C@users.noreply.github.com>
2023-07-16 00:46:32 +0800

    Update Symbolab.js

--- Symbolab.js
+++ Symbolab.js
@@ -2,7 +2,7 @@
 脚本功能：Symbolab Pro 解锁
 软件版本：最新
 下载地址：https://apps.apple.com/cn/app/symbolab%E8%AE%A1%E7%AE%97%E5%99%A8/id876942533
-脚本作者：Henry Chan
+脚本作者：Kaleidoscope-C
 更新时间：2023.07.16
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
