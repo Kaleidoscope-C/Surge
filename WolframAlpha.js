d2f53f84298b7437a637fe14166e2d6944f41861
Kaleidoscope-C <52686914+Kaleidoscope-C@users.noreply.github.com>
2023-07-16 01:46:23 +0800

    Update and rename WolframAlpha1.js to WolframAlpha.js

--- WolframAlpha.js
+++ WolframAlpha.js
@@ -1,7 +1,7 @@
 /*
 脚本功能：WolframAlpha Pro 解锁
 软件版本：最新
-下载地址：
+下载地址：https://apps.apple.com/us/app/wolframalpha/id548861535
 脚本作者：Kaleidoscope-C
 更新时间：2023.07.16
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
@@ -10,7 +10,7 @@
 
 [rewrite_local]
 # WolframAlpha Pro 解锁
-^https:\/\/www\.wolframalpha\.com\/users\/me\/account\?appid=* url script-response-body https://raw.githubusercontent.com/Kaleidoscope-C/Surge/main/WolframAlpha1.js
+^https:\/\/www\.wolframalpha\.com\/users\/me\/account\?appid=* url script-response-body https://raw.githubusercontent.com/Kaleidoscope-C/Surge/main/WolframAlpha.js
 
 [mitm] 
 hostname = www.wolframalpha.com
