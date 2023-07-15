0d51daac515a4ae4d9d71368347e52ff3ef19431
Kaleidoscope-C <52686914+Kaleidoscope-C@users.noreply.github.com>
2023-07-16 00:45:15 +0800

    Create Symbolab.js

---
+++ Symbolab.js
@@ -0,0 +1,34 @@
+/*
+脚本功能：Symbolab Pro 解锁
+软件版本：最新
+下载地址：https://apps.apple.com/cn/app/symbolab%E8%AE%A1%E7%AE%97%E5%99%A8/id876942533
+脚本作者：Henry Chan
+更新时间：2023.07.16
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+
+********************************
+
+[rewrite_local]
+
+# Symbolab 解锁订阅
+^https?:\/\/scibug\.com\/appleSubscriptionValidate$ url script-response-body https://raw.githubusercontent.com/harry-sunhao/QuanX/main/Symbolab/symbolab.js
+
+[mitm] 
+
+hostname = scibug.com
+*
+*
+*/
+
+
+let obj = JSON.parse($response.body);
+
+obj= {
+       "valid":true,
+       "hasUserConsumedAppleFreeTrial":false,
+       "isCurrentlyInFreeTrial":false,
+       "newlyAssociated":false
+}
+
+$done({body: JSON.stringify(obj)});
